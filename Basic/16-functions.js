e5dfa892d426b2442670358890d4a698628b64e2
M4rl0n274 <marlonquinteri274@gmail.com>
2025-03-19 23:31:07 -0500

    funciones ejercicios

---
+++ Basic/16-functions.js
@@ -0,0 +1,113 @@
+// Funciones
+
+//* Simple
+
+function myFunc() {
+  console.log("Hola, función! ");
+}
+
+for (let i = 0; i < 5; i++) {
+  myFunc();
+}
+
+//* Con parametros
+
+function myFuncWithParams(name) {
+  console.log(`¡Hola, ${name}!`);
+}
+myFuncWithParams("Marlon");
+myFuncWithParams("Marlon2");
+
+//* Funciones anónimas
+// hay que asignarla a una constante o variable
+const myFun2 = function (name) {
+  console.log(`¡Hola, ${name}!`);
+};
+
+myFun2("Marlon Quintero, funcion anonima");
+
+//* Arrow functions
+// hay que asignarlas a una constante o variable
+const myFunc3 = (name) => {
+  console.log(`¡Hola, ${name}!`);
+};
+
+const myFunc4 = (name) => console.log(`¡Hola, ${name}!`);
+
+myFunc3("MArlon Arrow functions");
+myFunc4("MArlon Arrow functions");
+
+// Parámetros
+
+function sum(a, b) {
+  console.log(a + b);
+}
+sum(5, 10);
+sum(5);
+sum();
+
+function defaultSum(a = 0, b = 0) {
+  console.log(a + b);
+}
+
+// Por defecto
+defaultSum();
+defaultSum(5);
+defaultSum(5, 10);
+defaultSum((b = 5));
+
+// Retorno de valores
+
+function mult(a, b) {
+  return a * b;
+}
+
+console.log(mult(5, 10));
+let resul = mult(5, 10);
+console.log(resul);
+
+// Funciones anidadas
+
+function extern() {
+    console.log("Funcion Externa");
+  function intern() {
+    console.log("Funcion interna");
+  }
+  intern()
+}
+extern()
+// intern() Fuera del scope 
+
+// Funciones de orden superior
+
+function applyFunc(func, param){
+    func(param)
+}
+
+applyFunc(myFunc4 ,"Funcion de orden superior")
+
+// forEach
+    // no se pueden detener con continue y break y no pueden retornar un nuevo array o set
+
+myArray = [ 1,2,3,4]
+
+mySet = new Set(["Marlon" ,  "Quintero","hola", 20, true, "marlon@gmail.com"])
+
+myMap = new Map ([
+    ["name", "Marlon"],
+    ["email", "marlon@gamil.com"],
+    ["age", 20]
+])
+
+
+myArray.forEach(function (value) {
+ console.log(value)
+})
+
+myArray.forEach((value) => console.log(value));
+
+mySet.forEach((value) => console.log(value));
+
+myMap.forEach((value) => console.log(value));
+
+
---
+++ Basic/17-functions-exercises.js
@@ -0,0 +1,94 @@
+// NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios
+
+//* 1. Crea una función que reciba dos números y devuelva su suma
+
+// function suma(a, b) {
+//   return a + b;
+// }
+// console.log(suma(10, 20));
+
+//* 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+// console.log("Ejercicio 2");
+
+// let numeros = [10, 20, 30, 100, 200, 1000];
+
+// let mayor;
+
+// function mayorArray(numeros) {
+//   for (let i = 0; i < numeros.length; i++) {
+//     //console.log(numeros[i]);
+//     for (let x = 1; x < numeros.length; x++) {
+//       //console.log(numeros[x]);
+
+//       if (numeros[i] > numeros[x]) {
+//         console.log(`El numero ${numeros[i]} es mayor que ${numeros[x]}`);
+//       }
+//     }
+//   }
+// }
+// console.log(mayorArray(numeros));
+
+//* 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
+
+// let vocales = "aeiouAEIOU";
+// let contador = 0;
+
+// function countVocals(string) {
+//   for (let i = 0; i < string.length; i++) {
+//     if (vocales.includes(string[i])) {
+//       contador++;
+//     }
+//   }
+//   console.log(
+//     `El numero de vocales para la palabra ${string} es de ${contador}`
+//   );
+// }
+// countVocals("Marlon");
+
+//* 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
+
+// let palabras = ["marlon", "desarrollador"];
+
+// function arrayMayusculas(Array) {
+//   let arregloEnMayusculas = [];
+//   for (let i = 0; i < Array.length; i++) {
+//     //arreglo.push(Array[i].tolowerCase()); // devuelve en mayusculas
+//     arregloEnMayusculas.push(Array[i].toUpperCase()); // devuelve en mayusculas
+//   }
+
+//   console.log(arregloEnMayusculas);
+// }
+
+// arrayMayusculas(palabras);
+
+//* 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
+
+function primo(n) {
+  if (n < 2) return false
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false; // Si tiene un divisor, no es primo
+    }
+  }
+  return true; // Si no tuvo divisores, es primo
+
+
+}
+console.log(primo(3))
+
+
+
+
+
+
+
+//* 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+
+//* 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
+
+//* 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
+
+//* 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
+
+//* 10. Crea una función que calcule el factorial de un número dado
