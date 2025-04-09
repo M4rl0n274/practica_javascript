da74499f8da68441431e308b3829ffc4e75fccf0
M4rl0n274 <marlonquinteri274@gmail.com>
2025-04-08 22:59:09 -0500

    ejercicios try catch

--- Basic/25-error-handing-exercises.js
+++ Basic/25-error-handing-exercises.js
@@ -1,8 +1,40 @@
-// 1. Captura una excepción utilizando try-catch
+//* 1. Captura una excepción utilizando try-catch
 
-// 2. Captura una excepción utilizando try-catch y finally
+// let computer = {
+//   ssd: "1tb",
+//   ram: "32gb",
+//   fuentePoder: "500W",
+// };
 
-// 3. Lanza una excepción genérica
+try {
+  console.log(computer.hdd);
+} catch {
+  console.log("se produjo un error");
+}
+
+//* 2. Captura una excepción utilizando try-catch y finally
+console.log("===Ejercicio 2===");
+
+try {
+  console.log(computer.cpu);
+} catch (error) {
+  console.log("Error en impresion", error.message);
+} finally {
+  console.log("El computador siempre tiene una cpu");
+}
+
+//* 3. Lanza una excepción genérica
+console.log("===Ejercicio 3===");
+
+function comprobacion(precio1, precio2) {
+  if (precio1 > precio2) {
+    throw new TypeError("No aplica el descuento "); 
+  }
+
+  return precio1 + precio2;
+}
+
+console.log(comprobacion(100, 200));
 
 // 4. Crea una excepción personalizada
 
