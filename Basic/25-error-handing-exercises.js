4f8e3bf502a3b6b63f54e50fcaf2fad37750c146
M4rl0n274 <marlonquinteri274@gmail.com>
2025-04-16 23:46:04 -0500

    ejercicios try catch

--- Basic/25-error-handing-exercises.js
+++ Basic/25-error-handing-exercises.js
@@ -96,8 +96,20 @@ try{
 
 //* 7. Captura varias excepciones en un mismo try-catch
 
+try{
+}
+catch{
+  
+}
 //* 8. Crea un bucle que intente transformar a float cada valor y capture y muestre los errores
 
 //* 9. Crea una función que verifique si un objeto tiene una propiedad específica y lance una excepción personalizada
 
 //* 10. Crea una función que realice reintentos en caso de error hasta un máximo de 10
+
+
+
+
+
+
+
