6cd631be9dbeea0a241ef43099c95d7d3775e5a0
M4rl0n274 <marlonquinteri274@gmail.com>
2025-05-02 23:50:27 -0500

    ejercicios modulos

--- Basic/32-modules-exercises.js
+++ Basic/32-modules-exercises.js
@@ -1,6 +1,23 @@
-import { saludo } from "./31-modules-exercises.js";
+import { saludo, nombre, persona, Computador, Cliente } from "./31-modules-exercises.js";
 
 //* 1.exporta una función
 console.log(saludo("Marlon"));
+
+//* 2. Exporta una constante
+
+console.log(nombre);
+
+//* 3. Exporta una clase
+
+let persona1 = new persona ("Marlon", 20, "Don Marlon")
+console.log(persona1);
+
+//* 4. Importa una función
+
+console.log(Computador("Marlon"));
+
+//* 5. Importa una constante
+
+console.log(Cliente);
 
 
