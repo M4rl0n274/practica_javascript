1bf07812b9787ea29489a50e2955a6fbb901aab1
M4rl0n274 <marlonquinteri274@gmail.com>
2025-05-27 15:49:48 -0500

    For desde hasta

---
+++ Ejercicios-desafio-latam/7.Ciclos/9.For desde hasta.js
@@ -0,0 +1,13 @@
+// Ejercicio
+// Crea la función mostrarNumeros que reciba dos parámetro, inicio y fin.
+
+// La función debe mostrar en consola todos los números desde el inicio hasta el fin.
+
+function mostrarNumeros(inicio, fin) {
+
+  for (let x = inicio; x <= fin; x++) {
+    console.log(x);
+  }
+}
+mostrarNumeros(4, 9);
+mostrarNumeros(20, 25);
