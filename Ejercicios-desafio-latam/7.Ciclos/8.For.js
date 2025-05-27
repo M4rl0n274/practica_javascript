ca99e9315799fd3c224574c61b2741ec88c368cc
M4rl0n274 <marlonquinteri274@gmail.com>
2025-05-27 15:46:32 -0500

    For

---
+++ Ejercicios-desafio-latam/7.Ciclos/8.For.js
@@ -0,0 +1,10 @@
+// Ejercicio
+// Crea una función llamada introduccionAFor que muestre en consola los números del 1 al 15 utilizando la instrucción for.
+
+function introduccionAFor() {
+  for (let x = 1; x <= 15; x++) {
+    console.log(x);
+  }
+}
+
+introduccionAFor();
